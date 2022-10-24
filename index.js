6a5e4b3d334c96b5ace6cb3a64ad10beee2dc366
BeatyWonder <beatymaina0@gmail.com>
2022-10-24 14:22:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let count = 0
+let newScore = document.getElementById("home")
+let guestScore = document.getElementById("Guest")
+
+function homeOne()
+{
+      newScore.textContent= count +=1
+}
+
+function homeTwo()
+{
+      newScore.textContent= count +=2
+}
+
+function homeThree()
+{
+      newScore.textContent= count +=3
+}
+
+function guestOne()
+{
+      guestScore.textContent= count +=1
+} 
+
+function guestTwo()
+{
+      guestScore.textContent= count +=2
+} 
+
+function guestThree()
+{
+      guestScore.textContent= count +=3
+} 
